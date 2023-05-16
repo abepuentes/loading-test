641c14275ea9def5c95a62f9aa16958a5fd3dc89
abepuentes <abe.puentes@gmail.com>
2023-05-16 10:19:43 -0400

    fix: actual path

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -18,14 +18,14 @@ export default async function Home() {
     >
       <strong style={{ color: "orange" }}>{t("import1")}</strong>
       <div className={styles.card}>
-        <Link href="/items">{`TO ITEMS ->`}</Link>
+        <Link href="/same">{`TO ITEMS ->`}</Link>
       </div>
       <br />
       <br />
       <br />
       <strong style={{ color: "purple" }}>{t("import2")}</strong>
       <div className={styles.card}>
-        <Link href="/items">{`TO ITEMS ->`}</Link>
+        <Link href="/different">{`TO ITEMS ->`}</Link>
       </div>
     </main>
   );
