0b19fba4c3456fc006e09e5863975fa70dcf3c0a
abepuentes <abe.puentes@gmail.com>
2023-05-16 09:50:48 -0400

    adding translation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,12 @@
+import { getTranslations } from "next-intl/server";
 import styles from "./page.module.css";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const t = await getTranslations("Page");
   return (
     <main className={styles.main}>
+      {t("home")}
       <div className={styles.card}>
         <Link href="/items">{`TO ITEMS ->`}</Link>
       </div>
