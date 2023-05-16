5d3bbdaf171c2278145d6d0c3f424d015c1e3043
abepuentes <abe.puentes@gmail.com>
2023-05-16 09:55:23 -0400

    adding a phrase

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,8 +5,18 @@ import Link from "next/link";
 export default async function Home() {
   const t = await getTranslations("Page");
   return (
-    <main className={styles.main}>
-      {t("home")}
+    <main
+      className={styles.main}
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        padding: "1rem",
+      }}
+    >
+      <strong>{t("home")}</strong>
       <div className={styles.card}>
         <Link href="/items">{`TO ITEMS ->`}</Link>
       </div>
